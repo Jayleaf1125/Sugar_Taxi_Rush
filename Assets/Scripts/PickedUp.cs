1ff4086403144e2d7a0586b1008579281270f699
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2024-10-25 23:29:16 -0400

    create: rough point system

--- Assets/Scripts/PickedUp.cs
+++ Assets/Scripts/PickedUp.cs
@@ -1,22 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PickedUp : MonoBehaviour
 {
     public GameObject collectiblePrefab;
+    public TextMeshProUGUI pointSystemScore;
     public int collectibleValue;
 
     private void Awake()
     {
-    
+        pointSystemScore = pointSystemScore.GetComponent<TextMeshProUGUI>();    
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -27,9 +30,21 @@ public class PickedUp : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
+
         if (collision.gameObject.tag == "Player")
         {
+        
+             int score = Convert.ToInt32(pointSystemScore.text);
+             UpdateScore(score);
+
             Destroy(collectiblePrefab);
         }
+    }
+
+    private void UpdateScore(int score)
+    {
+        score += collectibleValue;
+        pointSystemScore.text = Convert.ToString(score);
     }
 }
