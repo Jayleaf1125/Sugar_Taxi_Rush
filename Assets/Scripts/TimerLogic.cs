1675ed9a0493d70379e500996f7170dad500b0f8
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2024-10-30 23:09:46 -0400

    created: countdown timer

--- Assets/Scripts/TimerLogic.cs
+++ Assets/Scripts/TimerLogic.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class TimerLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI countdownTimer;
+    [SerializeField] float remainingTime;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }  else if (remainingTime < 0) 
+        {
+            remainingTime = 0;
+            countdownTimer.color = Color.red;
+        }
+
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        countdownTimer.text = string.Format("{0:00}: {1:00}", minutes, seconds);
     }
 }
