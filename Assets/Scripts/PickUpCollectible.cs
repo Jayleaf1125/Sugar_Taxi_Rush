b8f65fd980cadf5c2bfeacc41c45b1695c5c5723
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2024-10-30 21:26:57 -0400

    create: point system based on regular candy
    I created a point system in which the score is updated every time the player picks up regular candy.

--- Assets/Scripts/PickUpCollectible.cs
+++ Assets/Scripts/PickUpCollectible.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
 public class PickUpCollectible : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public TextMeshProUGUI score;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.gameObject.tag == "Coin")
+        if (other.CompareTag("Regular Candy"))
         {
-            Debug.Log("Coin was collected");
+            Debug.Log("Candy was collected was collected");
+            HandleRegularCandyPickUp(other);
+        }
+    }
+
+    void HandleRegularCandyPickUp(Collider2D other)
+    {
+        RegularCandyStats regularCandyStats = other.GetComponent<RegularCandyStats>();
+
+        if (regularCandyStats != null)
+        {
+            int score = Convert.ToInt32(this.score.text);
+            score += regularCandyStats.collectibleValue;
+            this.score.text = Convert.ToString(score);
         }
     }
 }
